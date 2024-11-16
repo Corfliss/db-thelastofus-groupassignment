33157b1c7278c64b686d81635b737ff0be16332b
Naim Baziz <naim.baziz220@gmail.com>
2024-11-16 22:45:04 +0700

    add of CR testimonal R discount and C voucher

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,36 @@
 from django.urls import path
-from main.views import show_main, register, login_user, logout_user, home, subcategory, profile, service_booking, mypay, mypay_transaction, service_job, service_job_status
+from main.views import (
+    show_main,
+    register,
+    login_user,
+    logout_user,
+    home,
+    subcategory,
+    profile,
+    service_booking,
+    mypay,
+    mypay_transaction,
+    service_job,
+    service_job_status,
+    create_testimonial,
+    discount,
+)
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
-    path('home/', home, name='home'),
-    path('subcategory/', subcategory, name='subcategory'),
-    path('profile/', profile, name='profile'),
-    path('mypay/', mypay, name='mypay'),
-    path('mypay-transaction/', mypay_transaction, name='mypay-transaction'),
-    path('service-job/', service_job, name='service-job'),
-    path('service-job-status/', service_job_status, name='service-job-status'),
-    path('service_booking/', service_booking, name="service_booking")
-]   
+    path("", show_main, name="show_main"),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
+    path("home/", home, name="home"),
+    path("subcategory/", subcategory, name="subcategory"),
+    path("profile/", profile, name="profile"),
+    path("mypay/", mypay, name="mypay"),
+    path("mypay-transaction/", mypay_transaction, name="mypay-transaction"),
+    path("service-job/", service_job, name="service-job"),
+    path("service-job-status/", service_job_status, name="service-job-status"),
+    path("service_booking/", service_booking, name="service_booking"),
+    path("create_testimonial/", create_testimonial, name="create_testimonial"),
+    path("discount/", discount, name="discount"),
+]
--- main/views.py
+++ main/views.py
@@ -12,13 +12,14 @@ from django.core import serializers
 import datetime
 
 
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def show_main(request):
     context = {
-        'title' : 'Welcome to Sijarta',
-        'last_login': request.COOKIES['last_login'],
+        "title": "Welcome to Sijarta",
+        "last_login": request.COOKIES["last_login"],
     }
     return render(request, "main.html", context)
+
 
 def register(request):
     form = UserCreationForm()
@@ -27,94 +28,97 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+            messages.success(request, "Your account has been successfully created!")
+            return redirect("main:login")
+    context = {"form": form}
+    return render(request, "register.html", context)
+
 
 def login_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
+            response.set_cookie("last_login", str(datetime.datetime.now()))
             return response
     else:
         form = AuthenticationForm()  # Ensure form is initialized for GET requests
 
-    context = {'form': form}
-    return render(request, 'login.html', context)
+    context = {"form": form}
+    return render(request, "login.html", context)
 
 
 def logout_user(request):
     logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
-    response.delete_cookie('last_login')
+    response = HttpResponseRedirect(reverse("main:login"))
+    response.delete_cookie("last_login")
     return response
 
+
 def home(request):
-    context = {
-        'title' : 'Sijarta Homepage'
-    }
+    context = {"title": "Sijarta Homepage"}
     return render(request, "home.html", context)
 
+
 def subcategory(request):
-    context = {
-        'title' : 'Sijarta Subcategory'
-    }
+    context = {"title": "Sijarta Subcategory"}
     return render(request, "subcategory.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def profile(request):
-    context = {
-        'title' : 'Sijarta Profile'
-    }
+    context = {"title": "Sijarta Profile"}
     return render(request, "profile.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def mypay(request):
-    past_transactions = [ {'amount': '+50.00', 'date': '2024-11-01', 'category': 'Deposit'},
-            {'amount': '-30.00', 'date': '2024-11-05', 'category': 'Withdrawal'},
-            {'amount': '+100.00', 'date': '2024-11-10', 'category': 'Deposit'},
-            ]
+    past_transactions = [
+        {"amount": "+50.00", "date": "2024-11-01", "category": "Deposit"},
+        {"amount": "-30.00", "date": "2024-11-05", "category": "Withdrawal"},
+        {"amount": "+100.00", "date": "2024-11-10", "category": "Deposit"},
+    ]
 
     context = {
-        'user' : request.user,
-        'phone_number': '123-456-789',
-        'balance': 12000,
-        'transactions': past_transactions
+        "user": request.user,
+        "phone_number": "123-456-789",
+        "balance": 12000,
+        "transactions": past_transactions,
     }
 
     return render(request, "mypay.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def mypay_transaction(request):
-    context = {
-        'user' : request.user
-    }
+    context = {"user": request.user}
 
     return render(request, "mypaytransaction.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def service_job(request):
-    context = {
-        'user' : request.user
-    }
+    context = {"user": request.user}
 
     return render(request, "servicejob.html", context)
 
 
 def service_job_status(request):
-    context = {
-        'user' : request.user
-    }
+    context = {"user": request.user}
 
     return render(request, "servicejobstatus.html", context)
 
+
 def service_booking(request):
-    context = {
-        'title' : 'Sijarta Service Booking'
-    }
+    context = {"title": "Sijarta Service Booking"}
     return render(request, "service_booking.html", context)
+
+
+def create_testimonial(request):
+    return render(request, "create_testimonial.html")
+
+
+def discount(request):
+    return render(request, "discount.html")
